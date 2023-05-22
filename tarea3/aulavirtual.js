6eb39303f557c867bc640ab500e48a199268f589
mastermarketmaker <mastermarketmaker@hotmail.com>
2023-05-23 00:02:59 +0200

    2

--- tarea3/aulavirtual.js
+++ tarea3/aulavirtual.js
@@ -1,19 +1,32 @@
+var usuario = {
+  nombre: 'Antonio Otero',
+  email: 'luisuper@hotmail.com',
+  usuario: 'antonio',
+  contraseña: 'antonio123'
+};
 
-    function login() {
-      var username = document.getElementById("username").value;
-      var password = document.getElementById("password").value;
-      
-      // Aquí puedes agregar la lógica para validar el nombre de usuario y la contraseña
-      // y redirigir al usuario a la página principal si son válidos.
-    }
-  
-    function forgotPassword() {
-      var email = prompt("Por favor, introduce tu dirección de correo electrónico:");
-      
-      // Aquí puedes agregar la lógica para verificar si el correo electrónico está en la base de datos
-      // y enviar un método de recuperación de usuario y contraseña al correo proporcionado.
-      
-      if (email) {
-        alert("Si el correo electrónico consta en nuestra base de datos, te enviaremos un método de recuperación de usuario y contraseña.");
-      }
-    }
+function login() {
+  var usernameInput = document.getElementById("username").value;
+  var passwordInput = document.getElementById("password").value;
+
+  // Verificar si el nombre de usuario y la contraseña ingresados coinciden con los del 'usuario'
+  if (usernameInput === usuario.usuario && passwordInput === usuario.contraseña) {
+    // Si coinciden, redirigir al usuario a la página del aula virtual
+    alert("Inicio de sesión exitoso!");
+    window.location.href = "contacto.html";
+  } else {
+    // Si no coinciden, mostrar un mensaje de error
+    alert("El nombre de usuario o la contraseña son incorrectos. Por favor, inténtalo de nuevo.");
+  }
+}
+
+function forgotPassword() {
+  var email = prompt("Por favor, introduce tu dirección de correo electrónico:");
+
+  // Verificar si el correo electrónico ingresado coincide con el correo electrónico del usuario
+  if (email === usuario.email) {
+    alert("Se han enviado instrucciones a tu correo electrónico " + email);
+  } else {
+    alert("Lo siento, el correo electrónico no existe. Por favor, inténtalo de nuevo.");
+  }
+}
